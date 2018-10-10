4613f60f89762a8a11216308349504ae97d3f879
Imran Pollob <polboy777@gmail.com>
2018-10-10 22:48:00 +0600

    bootstrap added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./index.css";
 import Search from "./Search.js";
 import Profile from "./Profile.js";
+import "bootstrap/dist/css/bootstrap.css";
 
 const rootElement = document.getElementById("root");
 
